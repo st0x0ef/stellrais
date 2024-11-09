b65203038f5a2b3500d1cedc14138f771260cb1b
TATHAN <costa.tathan@gmail.com>
2024-11-09 14:10:49 +0100

    Fix GUI sprites

--- common/src/main/java/com/st0x0ef/stellaris/client/screens/GUISprites.java
+++ common/src/main/java/com/st0x0ef/stellaris/client/screens/GUISprites.java
@@ -5,20 +5,20 @@ import net.minecraft.resources.ResourceLocation;
 
 public class GUISprites {
 
-    public static final ResourceLocation WATER_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/water_overlay.png");
-    public static final ResourceLocation WATER_SEPARATOR_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/water_separator_overlay.png");
-    public static final ResourceLocation HYDROGEN_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/hydrogen_gui_overlay.png");
-    public static final ResourceLocation OXYGEN_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/oxygen_gui_overlay.png");
-    public static final ResourceLocation SIDEWAYS_ENERGY_FULL = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/sideway_energy_full.png");
-    public static final ResourceLocation SIDEWAYS_ENERGY_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/sideway_battery_overlay.png");
-    public static final ResourceLocation LIQUID_TANK_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/water_tank_overlay.png");
-    public static final ResourceLocation OIL_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/oil_gui_overlay.png");
-    public static final ResourceLocation FUEL_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/fuel_overlay.png");
-    public static final ResourceLocation BATTERY_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/battery_overlay.png");
-    public static final ResourceLocation ENERGY_FULL = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/energy_full.png");
+    public static final ResourceLocation WATER_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/water_overlay");
+    public static final ResourceLocation WATER_SEPARATOR_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/water_separator_overlay");
+    public static final ResourceLocation HYDROGEN_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/hydrogen_gui_overlay");
+    public static final ResourceLocation OXYGEN_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/oxygen_gui_overlay");
+    public static final ResourceLocation SIDEWAYS_ENERGY_FULL = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/sideway_energy_full");
+    public static final ResourceLocation SIDEWAYS_ENERGY_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/sideway_battery_overlay");
+    public static final ResourceLocation LIQUID_TANK_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/water_tank_overlay");
+    public static final ResourceLocation OIL_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/oil_gui_overlay");
+    public static final ResourceLocation FUEL_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/fuel_overlay");
+    public static final ResourceLocation BATTERY_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/battery_overlay");
+    public static final ResourceLocation ENERGY_FULL = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/energy_full");
     public static final ResourceLocation LIT_PROGRESS_SPRITE = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/fire_full");
-    public static final ResourceLocation FLUID_TANK_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/fluid_tank_overlay.png");
-    public static final ResourceLocation NO_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/gui/util/no_overlay.png");
+    public static final ResourceLocation FLUID_TANK_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/fluid_tank_overlay");
+    public static final ResourceLocation NO_OVERLAY = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "util/no_overlay");
     public static final ResourceLocation SPACESUIT_OXYGEN_BAR = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/overlay/oxygen_hud.png");
     public static final ResourceLocation SPACESUIT_FUEL_BAR = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/overlay/fuel_hud.png");
     public static final ResourceLocation SPACESUIT_ENERGY_BAR = ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/overlay/energy_hud.png");

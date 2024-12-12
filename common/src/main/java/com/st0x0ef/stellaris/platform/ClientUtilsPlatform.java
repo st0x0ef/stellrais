a3ba9e9786ca330c7fe44884a50797e139cf44a2
eirmax <eirmax8@gmail.com>
2024-12-12 21:31:08 +0700

    UtilPlatform

--- common/src/main/java/com/st0x0ef/stellaris/platform/ClientUtilsPlatform.java
+++ common/src/main/java/com/st0x0ef/stellaris/platform/ClientUtilsPlatform.java
@@ -1,20 +1,21 @@
 package com.st0x0ef.stellaris.platform;
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.world.entity.Display;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public class ClientUtilsPlatform {
-    @FunctionalInterface
-    public interface ArmorFactory {
-        HumanoidModel<?> create(ModelPart root, EquipmentSlot slot, ItemStack stack, HumanoidModel<LivingEntity> parentModel);
+    public interface ArmorFactory<T extends HumanoidRenderState> {
+        HumanoidModel<T> create(ModelPart root, EquipmentSlot slot, ItemStack stack, HumanoidModel<T> parentModel);
     }
-
     @ExpectPlatform
     public static void registerArmor(ModelLayerLocation layer, ArmorFactory factory, Item... items) {
         throw new AssertionError();

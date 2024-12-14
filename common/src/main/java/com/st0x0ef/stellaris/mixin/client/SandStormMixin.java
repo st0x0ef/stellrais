eac45fd605833012771a7f22e21048a78db82551
TATHAN <costa.tathan@gmail.com>
2024-12-14 17:19:11 +0100

    Basic SandStorm

---
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/client/SandStormMixin.java
@@ -0,0 +1,48 @@
+package com.st0x0ef.stellaris.mixin.client;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.st0x0ef.stellaris.Stellaris;
+import com.st0x0ef.stellaris.common.registry.TagRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LevelRenderer.class)
+public class SandStormMixin {
+
+    @Final
+    @Shadow
+    @Mutable
+    private Minecraft minecraft;
+
+
+    @Inject(
+            method = "renderSnowAndRain",
+            at = @At(value = "INVOKE",
+                    target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/resources/ResourceLocation;)V",
+                    shift = At.Shift.AFTER,
+                    ordinal = 0
+            )
+    )
+    public void renderSandStorm(LightTexture lightTexture, float partialTick, double camX, double camY, double camZ, CallbackInfo ci, @Local BlockPos.MutableBlockPos mutableBlockPos) {
+         if(this.minecraft.level != null) {
+             Level level = this.minecraft.level;
+             if (level.getBiome(mutableBlockPos).is(TagRegistry.SANDSTORM_BIOMES_TAG)) {
+                 RenderSystem.setShaderTexture(0, ResourceLocation.fromNamespaceAndPath(Stellaris.MODID, "textures/environment/sandstorm.png"));
+             }
+         }
+    }
+
+}

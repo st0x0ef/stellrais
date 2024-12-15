70ea4d345d252a75aa771f2454092fa405a5aa76
TATHAN <costa.tathan@gmail.com>
2024-12-15 14:34:31 +0100

    Change Mixin to regular overlay

--- common/src/main/java/com/st0x0ef/stellaris/mixin/client/FogRenderMixin.java
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/client/FogRenderMixin.java
@@ -1,23 +1,10 @@
 package com.st0x0ef.stellaris.mixin.client;
 
-import com.st0x0ef.stellaris.Stellaris;
 import com.st0x0ef.stellaris.common.effects.SandStormEffect;
-import com.st0x0ef.stellaris.common.registry.EffectsRegistry;
-import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.Overlay;
 import net.minecraft.client.renderer.FogRenderer;
-import net.minecraft.client.renderer.ScreenEffectRenderer;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.block.PowderSnowBlock;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 
@@ -42,25 +29,4 @@ public class FogRenderMixin {
         }
     }
 
-    @Mixin(Gui.class)
-    public static class RenderSandStormOverlay {
-
-        @Final
-        @Shadow
-        @Mutable
-        private Minecraft minecraft;
-
-
-        @Inject(method = "renderCameraOverlays", at = @At("HEAD"))
-        private void renderSandStormOverlay(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
-            Gui gui = (Gui)(Object)this;
-
-            if (this.minecraft.player.hasEffect(EffectsRegistry.SANDSTORM)) {
-
-                MobEffectInstance instance = this.minecraft.player.getEffect(EffectsRegistry.SANDSTORM);
-
-                gui.renderTextureOverlay(guiGraphics, SandStormEffect.SANDSTORM_OVERLAY, instance.getBlendFactor(this.minecraft.player, deltaTracker.getGameTimeDeltaPartialTick(true)));
-            }
-        }
-    }
 }

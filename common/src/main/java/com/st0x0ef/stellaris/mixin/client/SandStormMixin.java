339db5d78d7f8014b66cdaecb7519226b2ae16ba
TATHAN <costa.tathan@gmail.com>
2024-12-15 14:47:43 +0100

    Added Wind sound

--- common/src/main/java/com/st0x0ef/stellaris/mixin/client/SandStormMixin.java
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/client/SandStormMixin.java
@@ -1,21 +1,25 @@
 package com.st0x0ef.stellaris.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.st0x0ef.stellaris.Stellaris;
 import com.st0x0ef.stellaris.common.registry.EffectsRegistry;
+import com.st0x0ef.stellaris.common.registry.SoundRegistry;
 import com.st0x0ef.stellaris.common.registry.TagRegistry;
 import com.st0x0ef.stellaris.common.utils.Utils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.FogRenderer;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.biome.Biome;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -52,6 +56,23 @@ public class SandStormMixin {
                  }
              }
          }
+    }
+
+    @WrapOperation(
+            method = "tickRain",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;playLocalSound(Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFZ)V")
+    )
+    private void bypassExpensiveCalculationIfNecessary(ClientLevel instance, BlockPos blockPos, SoundEvent soundEvent, SoundSource soundSource, float rainLevel, float v, boolean rainSoundTime, Operation<Void> original) {
+        if(this.minecraft.level != null) {
+            Level level = this.minecraft.level;
+            if (level.getBiome(blockPos).is(TagRegistry.SANDSTORM_BIOMES_TAG)) {
+                this.minecraft.level.playLocalSound(blockPos, SoundRegistry.WIND_SOUND.get(), SoundSource.WEATHER, 0.1F, 0.5F, false);
+
+            } else {
+                original.call(instance, blockPos, soundEvent, soundSource, rainLevel, v, rainSoundTime);
+            }
+        }
+
     }
 
 }

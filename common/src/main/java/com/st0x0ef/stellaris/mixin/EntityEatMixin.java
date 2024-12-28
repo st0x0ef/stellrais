a632beb18a8564b486cedc579a51b5cd4b9c82c4
eirmax <eirmax8@gmail.com>
2024-12-28 23:02:16 +0700

    small fix

--- common/src/main/java/com/st0x0ef/stellaris/mixin/EntityEatMixin.java
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/EntityEatMixin.java
@@ -3,13 +3,16 @@ package com.st0x0ef.stellaris.mixin;
 import com.st0x0ef.stellaris.common.registry.TagRegistry;
 import com.st0x0ef.stellaris.common.utils.PlanetUtil;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,6 +24,9 @@ public abstract class EntityEatMixin extends LivingEntity {
 
     @Shadow protected FoodData foodData;
 
+    @Unique
+    public abstract boolean stellaris$canEat(TagKey<Item> canAlwaysEat);
+
     protected EntityEatMixin(EntityType<? extends LivingEntity> entityType, Level level) {
         super(entityType, level);
     }
@@ -29,7 +35,7 @@ public abstract class EntityEatMixin extends LivingEntity {
     @Inject(at = @At(value = "HEAD"), method = "canEat")
     private void cancelEat(boolean canAlwaysEat, CallbackInfoReturnable<Boolean> cir) {
         if(PlanetUtil.isPlanet(level().dimension().location())) {
-            if(!PlanetUtil.hasOxygen(level()) && !food.is(TagRegistry.SPACE_FOOD)) {
+            if(!PlanetUtil.hasOxygen(level()) && !stellaris$canEat(TagRegistry.SPACE_FOOD)) {
                 cir.setReturnValue(false);
             }
         }

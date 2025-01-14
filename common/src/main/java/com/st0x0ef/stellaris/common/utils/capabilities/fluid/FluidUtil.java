cc195691d1bd3df82bc8b92707a771a0eb3c6311
Fej1Dev <guymoshegg007@gmail.com>
2025-01-14 20:46:04 +0200

    Update FluidUtil

--- common/src/main/java/com/st0x0ef/stellaris/common/utils/capabilities/fluid/FluidUtil.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/utils/capabilities/fluid/FluidUtil.java
@@ -1,6 +1,7 @@
 package com.st0x0ef.stellaris.common.utils.capabilities.fluid;
 
 import com.fej1fun.potentials.capabilities.Capabilities;
+import com.fej1fun.potentials.fluid.BaseFluidStorage;
 import com.fej1fun.potentials.fluid.UniversalFluidStorage;
 import dev.architectury.fluid.FluidStack;
 import net.minecraft.world.item.ItemStack;
@@ -19,6 +20,17 @@ public class FluidUtil {
 
         from.drain(inserted, false);
         to.fill(inserted, false);
+        return inserted;
+    }
+
+    /// ignores max fill/drain limits
+    public static FluidStack moveFluidWithSet(BaseFluidStorage from, BaseFluidStorage to, FluidStack stack) {
+        FluidStack inserted = FluidStack.create(stack ,to.fill(from.drain(stack, true), true));
+
+        if (inserted.isEmpty()) return FluidStack.empty();
+
+        from.drainWithoutLimits(inserted, false);
+        to.fillWithoutLimits(inserted, false);
         return inserted;
     }
 }

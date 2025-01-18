3059184e20d6a4b893134e3120db9a4ef0e6a2cb
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-01-18 14:44:39 -0500

    fix recipes errors

--- common/src/main/java/com/st0x0ef/stellaris/common/utils/capabilities/fluid/FluidUtil.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/utils/capabilities/fluid/FluidUtil.java
@@ -9,19 +9,23 @@ import net.minecraft.world.item.ItemStack;
 
 public class FluidUtil {
     public static FluidStack moveFluidToItem(int tank, UniversalFluidStorage from, ItemStack stackTo, long amount) {
+        if (stackTo.isEmpty()) return FluidStack.empty();
         UniversalFluidStorage to = Capabilities.Fluid.ITEM.getCapability(stackTo);
         if (to==null) return FluidStack.empty();
         return moveFluid(from, to, FluidStack.create(from.getFluidInTank(tank), amount));
     }
 
     public static FluidStack moveFluidFromItem(int tank,ItemStack stackFrom, UniversalFluidStorage to, long amount) {
+        if (stackFrom.isEmpty()) return FluidStack.empty();
         UniversalFluidStorage from = Capabilities.Fluid.ITEM.getCapability(stackFrom);
         if (from==null) return FluidStack.empty();
         return moveFluid(from, to, FluidStack.create(from.getFluidInTank(tank), amount));
     }
 
     public static FluidStack moveFluid(UniversalFluidStorage from, UniversalFluidStorage to, FluidStack stack) {
-        FluidStack inserted = FluidStack.create(stack ,to.fill(from.drain(stack, true), true));
+        if (stack.isEmpty()) return FluidStack.empty();
+
+        FluidStack inserted = FluidStack.create(stack, to.fill(from.drain(stack, true), true));
 
         if (inserted.isEmpty()) return FluidStack.empty();
 

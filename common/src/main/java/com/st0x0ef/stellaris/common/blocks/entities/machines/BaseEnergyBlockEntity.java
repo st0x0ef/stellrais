4e7e6cf517335af16d117bd7c04b81b5e3fc5bc8
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-01-31 12:25:16 -0500

    fix BaseEnergyBlockEntity

--- common/src/main/java/com/st0x0ef/stellaris/common/blocks/entities/machines/BaseEnergyBlockEntity.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/blocks/entities/machines/BaseEnergyBlockEntity.java
@@ -2,6 +2,7 @@ package com.st0x0ef.stellaris.common.blocks.entities.machines;
 
 import com.fej1fun.potentials.providers.EnergyProvider;
 import com.st0x0ef.stellaris.common.network.packets.SyncEnergyPacket;
+import com.st0x0ef.stellaris.common.network.packets.SyncEnergyPacketWithoutDirection;
 import com.st0x0ef.stellaris.common.utils.capabilities.energy.EnergyStorage;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.core.BlockPos;
@@ -24,9 +25,9 @@ public abstract class BaseEnergyBlockEntity extends BlockEntity implements Energ
             @Override
             protected void onChange() {
                 setChanged();
-                if (level != null && level.getServer() != null) {
+                if (level != null && level.getServer() != null && !level.getServer().getPlayerList().getPlayers().isEmpty()) {
                     NetworkManager.sendToPlayers(level.getServer().getPlayerList().getPlayers(),
-                            new SyncEnergyPacket(energyContainer.getEnergy(), getBlockPos(), null));
+                            new SyncEnergyPacketWithoutDirection(energyContainer.getEnergy(), getBlockPos()));
                 }
             }
         };

43b9e12575264940f24a7d2e86122e728cc4c234
eirmax <eirmax8@gmail.com>
2025-02-10 15:12:58 +0700

    final fix food

--- common/src/main/java/com/st0x0ef/stellaris/mixin/EntityEatMixin.java
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/EntityEatMixin.java
@@ -8,32 +8,30 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 @Mixin(Player.class)
 public abstract class EntityEatMixin extends LivingEntity {
-    @Shadow public abstract void playSound(SoundEvent soundEvent, float f, float g);
-
-    @Unique
-    public abstract boolean stellaris$canEat(TagKey<Item> canAlwaysEat);
+    @Shadow
+    public abstract void playSound(SoundEvent soundEvent, float f, float g);
 
     protected EntityEatMixin(EntityType<? extends LivingEntity> entityType, Level level) {
         super(entityType, level);
     }
 
 
-    @Inject(at = @At(value = "HEAD"), method = "canEat", cancellable = true)
-    private void cancelEat(boolean canAlwaysEat, CallbackInfoReturnable<Boolean> cir) {
-        if(level() instanceof ServerLevel serverLevel && !PlanetUtil.hasOxygenAt(serverLevel, getOnPos()) && !stellaris$canEat(TagRegistry.SPACE_FOOD)) {
-            cir.setReturnValue(canAlwaysEat);
+    @Unique
+    private void stellaris$cancelEat(Level level, ItemStack food, FoodProperties foodProperties, CallbackInfoReturnable<ItemStack> cir) {
+        if (level instanceof ServerLevel serverLevel && !PlanetUtil.hasOxygenAt(serverLevel, getOnPos()) && !food.is(TagRegistry.SPACE_FOOD)) {
+            cir.setReturnValue(food);
         }
     }
 }
